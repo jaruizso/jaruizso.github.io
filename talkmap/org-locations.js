2b94f9d915c73d6c753d9f0434dcfa3f4a4f38ef
Javier Ruiz Soler <javierruizsoler@MacBook-Air-de-Javier.local>
2017-07-26 12:50:41 +0200

    talkmap

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,1 +1,67 @@
-var addressPoints = [];
+var addressPoints = [
+  [
+    "Valencia, Spain", 
+    39.4699014, 
+    -0.3759513
+  ], 
+  [
+    "Budapest, Hungary", 
+    47.4983815, 
+    19.0404707
+  ], 
+  [
+    "Lisbon, Portugal", 
+    38.7077926, 
+    -9.1365061
+  ], 
+  [
+    "Tampere, Finland", 
+    61.4980214, 
+    23.7603118
+  ], 
+  [
+    "Berlin, Germany", 
+    52.5170365, 
+    13.3888599
+  ], 
+  [
+    "Milan, Italy", 
+    45.4667971, 
+    9.1904984
+  ], 
+  [
+    "Vilnius, Lithuania", 
+    54.6870458, 
+    25.2829111
+  ], 
+  [
+    "Loughborough, UK", 
+    52.7723859, 
+    -1.2077985
+  ], 
+  [
+    "Cologne, Germany", 
+    50.938361, 
+    6.959974
+  ], 
+  [
+    "Copenhagen, Denmark", 
+    55.6867243, 
+    12.5700724
+  ], 
+  [
+    "Istanbul, Turkey", 
+    41.0096334, 
+    28.9651646
+  ], 
+  [
+    "Malm\u00f6, Sweden", 
+    55.6052931, 
+    13.0001566
+  ], 
+  [
+    "London, UK", 
+    51.5073219, 
+    -0.1276474
+  ]
+];
